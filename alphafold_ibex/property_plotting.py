ed800e02633b8d2708de9b0ae3638c836ca6081b
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2022-12-03 12:02:21 +0300

    Modified plot titles to start ranking from 0

--- alphafold_ibex/property_plotting.py
+++ alphafold_ibex/property_plotting.py
@@ -28,7 +28,7 @@ def plot_paes(paes, chain_breaks=None, dpi=100, fig=True, savefile:Path=None):
     if fig: plt.figure(figsize=(3*num_models,2), dpi=dpi)
     for n,pae in enumerate(paes):
         plt.subplot(1,num_models,n+1)
-        plt.title(f"rank_{n+1}")
+        plt.title(f"rank_{n}")
         Ln = pae.shape[0]
         plt.imshow(pae,cmap="bwr",vmin=0,vmax=30,extent=(0, Ln, Ln, 0))
         if chain_breaks is not None and len(chain_breaks) > 1:
@@ -44,7 +44,7 @@ def plot_adjs(adjs, chain_breaks=None, dpi=100, fig=True, savefile:Path=None):
     if fig: plt.figure(figsize=(3*num_models,2), dpi=dpi)
     for n,adj in enumerate(adjs):
         plt.subplot(1,num_models,n+1)
-        plt.title(f"rank_{n+1}")
+        plt.title(f"rank_{n}")
         Ln = adj.shape[0]
         plt.imshow(adj,cmap="binary",vmin=0,vmax=1,extent=(0, Ln, Ln, 0))
         if chain_breaks is not None and len(chain_breaks) > 1:
@@ -61,7 +61,7 @@ def plot_dists(dists, chain_breaks=None, dpi=100, fig=True, savefile:Path=None):
     if fig: plt.figure(figsize=(3*num_models,2), dpi=dpi)
     for n,dist in enumerate(dists):
         plt.subplot(1,num_models,n+1)
-        plt.title(f"rank_{n+1}")
+        plt.title(f"rank_{n}")
         Ln = dist.shape[0]
         plt.imshow(dist,extent=(0, Ln, Ln, 0))
         if chain_breaks is not None and len(chain_breaks) > 1:
@@ -77,7 +77,7 @@ def plot_plddts(plddts, chain_breaks=None, dpi=100, fig=True, savefile:Path=None
     if fig: plt.figure(figsize=(8,5),dpi=dpi)
     plt.title("Predicted lDDT per position")
     for n,plddt in enumerate(plddts):
-        plt.plot(plddt,label=f"rank_{n+1} ({plddt.mean():.2f})")
+        plt.plot(plddt,label=f"rank_{n} ({plddt.mean():.2f})")
     if chain_breaks is not None:
         L_prev = 0
         for L_i in chain_breaks[:-1]:
