93d924dd42071cae06e618497b6e4a14477594da
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2023-06-07 02:06:30 +0300

    Fixed pdockq function

--- alphafold_ibex/scoring_functions.py
+++ alphafold_ibex/scoring_functions.py
@@ -11,6 +11,7 @@ import pickle
 import json
 from collections import defaultdict
 import math
+import pandas as pd
 
 from typing import Union, Tuple, List, Dict
 
@@ -40,7 +41,8 @@ def parse_atm_record(line):
 def read_pdb(pdbfile:str) -> Tuple[Dict[str,List[str]],
                                    Dict[str,List[List[float]]],
                                    Dict[str,List[int]],
-                                   Dict[str,List[int]]]:
+                                   Dict[str,List[int]],
+                                   Dict[str,np.ndarray]]:
     """
     Read a pdb file per chain
 
@@ -62,11 +64,15 @@ def read_pdb(pdbfile:str) -> Tuple[Dict[str,List[str]],
     - chain_CB_inds: Dictionary with the indices of the CB atoms for each chain.
             The keys are the chain ids and the values are lists of integers
             with the indices
+    - res_indices: Dictionary with the residue numbers for each chain. The keys
+            are the chain ids and the values are numpy arrays with the residue
+            index for each atom
     """
     pdb_chains = {}
     chain_coords = {}
     chain_CA_inds = {}
     chain_CB_inds = {}
+    res_indices = {}
 
     with open(pdbfile) as file:
         for line in file:
@@ -75,6 +81,7 @@ def read_pdb(pdbfile:str) -> Tuple[Dict[str,List[str]],
                 if record['chain'] in [*pdb_chains.keys()]:
                     pdb_chains[record['chain']].append(line)
                     chain_coords[record['chain']].append([record['x'],record['y'],record['z']])
+                    res_indices[record['chain']].append(record['res_no']-1)
                     coord_ind+=1
                     if record['atm_name']=='CA':
                         chain_CA_inds[record['chain']].append(coord_ind)
@@ -85,13 +92,17 @@ def read_pdb(pdbfile:str) -> Tuple[Dict[str,List[str]],
                 else:
                     pdb_chains[record['chain']] = [line]
                     chain_coords[record['chain']]= [[record['x'],record['y'],record['z']]]
+                    res_indices[record['chain']] = [record['res_no']-1]
                     chain_CA_inds[record['chain']]= []
                     chain_CB_inds[record['chain']]= []
                     #Reset coord ind
                     coord_ind = 0
 
+    # Convert residue numbers from list to np.array
+    for k,v in res_indices.items():
+        res_indices[k] = np.array(v)
 
-    return pdb_chains, chain_coords, chain_CA_inds, chain_CB_inds
+    return pdb_chains, chain_coords, chain_CA_inds, chain_CB_inds,res_indices 
 
 
 def get_best_plddt(work_dir:str) -> np.ndarray:
@@ -215,7 +226,10 @@ def calculate_mpDockQ(complex_score:float) -> float:
     return L/(1+math.exp(-1*k*(complex_score-x_0))) + b
 
 
-def calc_pdockq(chain_coords, chain_plddt, t) -> Tuple[float,float,int]:
+def calc_pdockq(chain_coords:Dict[str,List[List[float]]],
+                chain_plddt:Dict[str,np.ndarray],
+                t:int,
+                res_indices:Dict[str,np.ndarray]) -> Tuple[float,float,int]:
     """
     Calculate the pDockQ scores
     pdockQ = L / (1 + np.exp(-k*(x-x0)))+b
@@ -231,7 +245,10 @@ def calc_pdockq(chain_coords, chain_plddt, t) -> Tuple[float,float,int]:
         chain_plddt (dict): Dictionary with the pLDDT scores per chain. The
                 keys are the chain ids and the values are numpy arrays with
                 the pLDDT scores
-        t (float): Distance threshold for the interface
+        t (int): Distance threshold for the interface
+        res_indices (dict): Dictionary with the residue numbers for each chain.
+                The keys are the chain ids and the values are numpy arrays with
+                the residue index for each atom
     
     Returns:
         Tuple[float,float,int]: Tuple with the pDockQ score, the average pLDDT
@@ -255,9 +272,15 @@ def calc_pdockq(chain_coords, chain_plddt, t) -> Tuple[float,float,int]:
         pdockq=0
     else:
         #Get the average interface plDDT
-        avg_if_plddt = np.average(np.concatenate([plddt1[np.unique(contacts[:,0])], plddt2[np.unique(contacts[:,1])]]))
+        residue_indices_A = np.unique(res_indices[ch1][contacts[:,0]])
+        residue_indices_B = np.unique(res_indices[ch2][contacts[:,1]])
+        avg_if_plddt = np.average(np.concatenate([plddt1[residue_indices_A], 
+                                                  plddt2[residue_indices_B]]))
         #Get the number of interface contacts
-        n_if_contacts = contacts.shape[0]
+        # Convert the contacts to residue indexes, and remove duplicates
+        n_if_contacts = pd.DataFrame(
+            [[res_indices['A'][a], res_indices['B'][b]] for a,b in contacts]
+            ).drop_duplicates().shape[0]
         x = avg_if_plddt*np.log10(n_if_contacts)
         pdockq = 0.724 / (1 + np.exp(-0.052*(x-152.611)))+0.018
 
@@ -275,14 +298,14 @@ def obtain_mpdockq(work_dir:str) -> Union[Tuple[float,float,int], str]:
         Union[Tuple[float,float,int], str]: Quality scores for the complex
     """
     pdb_path = os.path.join(work_dir,'ranked_0.pdb')
-    pdb_chains, chain_coords, chain_CA_inds, chain_CB_inds = read_pdb(pdb_path)
+    pdb_chains, chain_coords, chain_CA_inds, chain_CB_inds, res_indices = read_pdb(pdb_path)
     best_plddt = get_best_plddt(work_dir)
     plddt_per_chain = read_plddt(best_plddt,chain_CA_inds)
     complex_score,num_chains = score_complex(chain_coords,chain_CB_inds,plddt_per_chain)
     if complex_score is not None and num_chains>2:
         mpDockq_or_pdockq = calculate_mpDockQ(complex_score)
     elif complex_score is not None and num_chains==2:
-        mpDockq_or_pdockq = calc_pdockq(chain_coords,plddt_per_chain,t=8)
+        mpDockq_or_pdockq = calc_pdockq(chain_coords,plddt_per_chain,8,res_indices)
     else:
         mpDockq_or_pdockq = "None"
     return mpDockq_or_pdockq
