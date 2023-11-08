b875bad24f1acd5c612574f3eddc22fa556bffd9
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2023-11-08 22:32:44 +0300

    Added code to do joint clustering instead of seq or str

--- bin/clustering.py
+++ bin/clustering.py
@@ -10,8 +10,10 @@ import shutil
 import subprocess
 import re
 from pymol import cmd
+from typing import Dict, Set
 
 import pandas as pd
+import numpy as np
 
 import logging
 logging.getLogger().setLevel(logging.INFO)
@@ -62,10 +64,6 @@ def parsing(args: list=None) -> argparse.Namespace:
 
     parser = argparse.ArgumentParser(description=('Copy the top structures and '
         'plots to other directories.'))
-    
-    parser.add_argument("--kind",
-        help=("Kind of clustering to perform. (Default=sequence)"), default="sequence",
-        choices=["sequence", "structure"])
     
     parser.add_argument("--models_dir", 
         help=('Path with the resulting AlphaFold models. Required if --kind=sequence.'),
@@ -194,11 +192,11 @@ def copy_pdbs(clusters:pd.DataFrame, models_dir:Path, destination:Path) -> None:
         destination (Path): Path to the directory where the pdbs will be copied
     """
     
-    top10 = clusters.rep.value_counts().sort_values(ascending=False).head(10)
+    top10 = clusters.merged_rep.value_counts().sort_values(ascending=False).head(10)
     
     for i, cluster in enumerate(top10.index):
         # Get cluster members
-        members = clusters[clusters.rep == cluster].member.values
+        members = clusters[clusters.merged_rep == cluster].member.values
         cluster_dir = destination / f"cluster{i+1}_{cluster}"
         cluster_dir.mkdir()
         for member in members:
@@ -214,13 +212,13 @@ def copy_pdbs(clusters:pd.DataFrame, models_dir:Path, destination:Path) -> None:
 
 def make_pymol_sessions(clusters:pd.DataFrame, destination:Path):
     
-    top10 = clusters.rep.value_counts().sort_values(ascending=False).head(10)
+    top10 = clusters.merged_rep.value_counts().sort_values(ascending=False).head(10)
     
     for i, cluster in enumerate(top10.index):
         # Get cluster members
-        members = clusters[clusters.rep == cluster].member.values
+        members = clusters[clusters.merged_rep == cluster].member.values
         cluster_dir = destination / f"cluster{i+1}_{cluster}"
-        
+
         # Load the cluster representative first
         if re.search(r'.pdb_[AB]$', cluster):
             chain = cluster[-1]
@@ -251,53 +249,179 @@ def make_pymol_sessions(clusters:pd.DataFrame, destination:Path):
         cmd.do('delete all')
 
 
+def get_topcluster_members(clusters:pd.DataFrame, min_count:int=2) -> Dict[str, Set[str]]:
+    """
+    Obtain the top clusters in dictionary format, with the cluster representative
+    as the key and the members as the values in a set.
+
+    Args:
+        clusters (pd.DataFrame): _description_
+        min_count (int, optional): _description_. Defaults to 2.
+
+    Returns:
+        dict: _description_
+    
+    E.g.
+    {'Q5N7Y5': {'Q5N7Y5', 'Q69XA8', 'Q6K7U3'},
+     'Q2QN41': {'Q2QN41', 'Q8W0W4'},
+     'A0A0P0XNZ0': {'A0A0P0XNZ0', 'A0A0P0XQ16'}}
+    """
+    # Get the member count
+    member_counts = (clusters.groupby('rep').agg({'member': 'count'})
+                 .sort_values(by='member', ascending=False).reset_index())
+    # Get the clusters with more than one member
+    top_clusters = member_counts[member_counts.member >= min_count]
+    # Get the members of the top clusters
+    top_clusters_members = {r:set(clusters[clusters.rep==r].member.tolist()) \
+                           for r in top_clusters.rep}
+    
+    return top_clusters_members
+
+
+def merge_dict_values(unmerged_dict:Dict[str, Set[str]]) -> Dict[str, Set[str]]:
+    """
+    Take a dictionary with sets as values, and merge the sets that have elements
+    in common.
+
+    Args:
+        d (Dict[str, Set[str]]): _description_
+
+    Returns:
+        Dict[str, Set[str]]: _description_
+    """
+    
+    joint_dict = {}
+    while unmerged_dict:
+        rep, members = unmerged_dict.popitem()
+
+        if members is None:
+            continue
+
+        merged_members = set()
+        keys_to_merge = []
+        for rep2, members2 in unmerged_dict.items():
+            if not members.isdisjoint(members2):    # if they have elements in common
+                merged_members = merged_members | members | members2
+                keys_to_merge.append(rep2)
+
+        # Erase the values of the merged clusters
+        for rep2 in keys_to_merge:
+            unmerged_dict[rep2] = None
+
+        if merged_members:
+            joint_dict[rep] = merged_members
+        else:
+            joint_dict[rep] = members
+    
+    return joint_dict
+
+
+def joint_cluster(seqclusters:pd.DataFrame, strclusters:pd.DataFrame) -> Dict[str, Set[str]]:
+    
+    top_seqclusters_members = get_topcluster_members(seqclusters)
+    top_strclusters_members = get_topcluster_members(strclusters)
+    
+    # Iterate over the top structure clusters, and see if they have elements in
+    # common with the top sequence clusters. If they do, join them.
+    joint_clusters = {}
+    merged_seqclusters = []
+    for rep, members in top_strclusters_members.items():
+        merged_members = set()
+        for rep2, members2 in top_seqclusters_members.items():
+            if not members.isdisjoint(members2):
+                merged_members = merged_members | members | members2
+                merged_seqclusters.append(rep2)
+        
+        if merged_members:
+            joint_clusters[rep] = merged_members
+
+    # Merge joint clusters further in cases where they have elements in common
+    joint_clusters2 = merge_dict_values(joint_clusters)
+
+    # Merge the rest of the structure clusters
+    joint_clusters2 = top_strclusters_members | joint_clusters2
+
+    # Merge the missing sequence clusters
+    missing_seqclusters = set(top_seqclusters_members.keys()) - set(merged_seqclusters)
+    if missing_seqclusters:
+        for rep in missing_seqclusters:
+            joint_clusters2[rep] = top_seqclusters_members[rep]
+    
+    return joint_clusters2
+    
+
+
 if __name__ == '__main__':
 
     args = parsing()
     
-    if args.kind=='sequence':
+    # Sequence clustering
+    out_seqcluster = args.destination / "seqclusters"
+    logging.info("Running clustering...")
+    run_sequence_clustering(args.candidates, "clusterRes", "tmp", args.min_seq_id,
+                args.coverage, args.cov_mode, args.sensitivity, out_seqcluster)
     
-        logging.info("Running clustering...")
-        run_sequence_clustering(args.candidates, "clusterRes", "tmp", args.min_seq_id,
-                    args.coverage, args.cov_mode, args.sensitivity, args.destination)
-        
-        logging.info("Processing output...")
-        clusters_tsv = args.destination / "clusterRes_cluster.tsv"
-        clusters = pd.read_table(clusters_tsv, header=None, names=["rep", "member"])
+    logging.info("Processing output...")
+    seqclusters_tsv = out_seqcluster / "clusterRes_cluster.tsv"
+    seqclusters = pd.read_table(seqclusters_tsv, header=None, names=["rep", "member"])
     
-    else:
-        
-        logging.info("Copying models to new directory...")
-        pdbs_dir = get_top_pdbs(args.models_dir, args.destination)
-        
-        logging.info("Running strcutural clustering...")
-        run_structure_clustering(pdbs_dir, "clusterRes", "tmp", args.coverage,
-                                 args.cov_mode, args.evalue, args.destination)
-        
-        logging.info("Processing output...")
-        clusters_tsv = args.destination / "clusterRes_cluster.tsv"
-        clusters = pd.read_table(clusters_tsv, header=None, names=["rep", "member"])
-        # Get only the clusters for the structures of the binder
-        clusters = clusters[clusters.member.str.endswith('_B')]
-        # See if all the representatives also come from chain B
-        if not all(clusters.rep.str.endswith('_B')):
-            logging.info("NOT ALL REPRESENTATIVES COME FROM CHAIN B")
-        # Remove the '.pdb_B' suffix from the members' names
-        clusters['member'] = clusters['member'].str.split('.pdb').str[0]
+    # Structure clustering
+    out_strcluster = args.destination / "strclusters"
+    logging.info("Copying models to new directory...")
+    pdbs_dir = get_top_pdbs(args.models_dir, out_strcluster)
     
+    logging.info("Running strcutural clustering...")
+    run_structure_clustering(pdbs_dir, "clusterRes", "tmp", args.coverage,
+                                args.cov_mode, args.evalue, out_strcluster)
+    
+    logging.info("Processing output...")
+    strclusters_tsv = out_strcluster / "clusterRes_cluster.tsv"
+    strclusters = pd.read_table(strclusters_tsv, header=None, names=["rep", "member"])
+    # Get only the clusters for the structures of the binder
+    strclusters = strclusters[strclusters.member.str.endswith('_B')]
+    # See if all the representatives also come from chain B
+    if not all(strclusters.rep.str.endswith('_B')):
+        logging.info("NOT ALL REPRESENTATIVES COME FROM CHAIN B")
+    # Remove the '.pdb_B' suffix from the members' names
+    strclusters['member'] = strclusters['member'].str.split('.pdb').str[0]
+    
+    # Joint clustering
+    joint_clusters = joint_cluster(seqclusters, strclusters)
+    
+    # Modify old columns
+    strclusters.rename(columns={'rep':'str_rep'}, inplace=True)
+    seqclusters.rename(columns={'rep':'seq_rep'}, inplace=True)
+
+    # Make the `complex` column the index
+    strclusters.set_index('complex', inplace=True)
+    seqclusters.set_index('complex', inplace=True)
+
+    # Add the `seq_rep` column to the `strclusters` dataframe
+    strclusters['seq_rep'] = seqclusters.seq_rep
+    
+    # Initialize a new column `merged_rep`
+    strclusters['merged_rep'] = np.nan
+    
+    # Iterate over joint_clusters and change the values of the `merged_rep` column
+    # in the `strclusters` dataframe
+    for rep, members in joint_clusters.items():
+        strclusters.loc[strclusters.member.isin(members), 'merged_rep'] = rep
+    
+    # Copy pdbs from the top clusters
+    out_merged = args.destination / "merged_clusters"
     logging.info("Copying pdbs from the top clusters...")
-    copy_pdbs(clusters, args.models_dir, args.destination)
+    copy_pdbs(strclusters, args.models_dir, out_merged)
     
     logging.info("Making Pymol sessions...")
-    make_pymol_sessions(clusters, args.destination)
-    
+    make_pymol_sessions(strclusters, out_merged)
+
+    # Add quality scores to the clusters
     logging.info("Adding quality scores to the clusters...")
     pdockq = pd.read_csv(args.models_dir / "scores/scores_pdockq.csv")
     pdockq['binder'] = pdockq.complex.str.split('_').str[1].str[:-2]
-    scores_clusters = pdockq.merge(clusters, how='left', left_on='binder',
-                                   right_on='member')
-    
-    logging.info("Saving results...")
-    scores_clusters.to_csv(args.destination / "scores_clusters.csv", index=False)
-    
+
+    strclusters_scores = pdockq.merge(strclusters, how='left', left_on='binder',
+                                right_on='member')
+    strclusters_scores.to_csv(out_merged / "scores_clusters.csv", index=False)
+
     logging.info("Done!!")
