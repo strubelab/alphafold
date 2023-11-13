579d9ff33aad5986b1f83cc63400e8167286dcba
Francisco J. Guzman <36437485+guzmanfj@users.noreply.github.com>
2023-11-13 19:05:27 +0300

    Refactor clustering code into more functions

--- bin/clustering.py
+++ bin/clustering.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 """
-Script to extract the quality scores the models made by AlphaFold
+This script performs the following actions to cluster structures:
+1. Run mmseqs easy-cluster to cluster the candidates by sequence identity
+2. Run foldseek easy-cluster to cluster the models by structure
+3. Merge the sequence and structure clusters
+4. Copy the PDBs from each joint cluster to a new directory
+5. Make Pymol sessions for the top clusters
 """
 
 import argparse
@@ -10,7 +15,7 @@ import shutil
 import subprocess
 import re
 from pymol import cmd
-from typing import Dict, Set
+from typing import Dict, Set, Union, Tuple
 
 import pandas as pd
 import numpy as np
@@ -100,16 +105,23 @@ def parsing(args: list=None) -> argparse.Namespace:
         help=('E-value for structural clustering. (Default=0.01)'), default=0.01,
         type=float)
     
+    parser.add_argument("--top_n",
+        help=('Number of top clusters to copy. (Default=10)'), default=10,
+        type=int)
+    
     return parser.parse_args(args)
 
 
-def run_sequence_clustering(candidates_file:Path, results_prefix:str="clusterRes",
-                   temp_dir:str="tmp", min_seq_id:float=0.3, coverage:float=0.8,
-                   cov_mode:int=2, sensitivity:int=7, destination:Path=Path(".")):
+
+def run_sequence_clustering(destination:Path, candidates_file:Path,
+                        results_prefix:str="clusterRes", temp_dir:str="tmp",
+                        min_seq_id:float=0.3, coverage:float=0.8, cov_mode:int=2,
+                        sensitivity:int=7):
     """
     Run mmseqs easy-cluster to cluster the candidates by sequence identity
 
     Args:
+        destination (Path): Parent destination path
         candidates_file (Path): Path to the FASTA file with the candidates
         results_prefix (str, optional): Prefix for the result files. Defaults to "clusterRes".
         temp_dir (str, optional): Defaults to "tmp".
@@ -117,36 +129,96 @@ def run_sequence_clustering(candidates_file:Path, results_prefix:str="clusterRes
         coverage (float, optional): Defaults to 0.8.
         cov_mode (int, optional): Defaults to 2.
         sensitivity (int, optional): Defaults to 7.
-        destination (Path, optional): Path to run the process from. Defaults to Path(".").
     """
     
+    out_seqcluster = destination / "seqclusters"
+    out_seqcluster.mkdir()
+
+    logging.info("Running sequence clustering...")
     command = (f"mmseqs easy-cluster {candidates_file} {results_prefix} {temp_dir} "
                f"--min-seq-id {min_seq_id} -c {coverage} --cov-mode {cov_mode} "
                f"-s {sensitivity}").split()
     
-    p = subprocess.run(command, cwd=destination)
+    p = subprocess.run(command, cwd=out_seqcluster)
 
+    logging.info("Processing output...")
+    seqclusters_tsv = out_seqcluster / "clusterRes_cluster.tsv"
+    seqclusters = pd.read_table(seqclusters_tsv, header=None, names=["rep", "member"])
 
-def run_structure_clustering(models_dir:Path, results_prefix:str="clusterRes",
+    return seqclusters
+    
+
+def run_structure_clustering(destination:Path, top_models_dir:Union[Path, None],
+                    models_dir:Path, results_prefix:str="clusterRes",
                     temp_dir:str="tmp", coverage:float=0.8, cov_mode:int=2,
-                    evalue:float=0.01, destination:Path=Path(".")):
+                    evalue:float=0.01) -> Tuple[pd.DataFrame, Path]:
     """
     Run foldseek easy-cluster to cluster the models by structure
 
     Args:
-        models_dir (Path): Path to the directory with the models to cluster
+        destination (Path): Parent destination path
+        top_models_dir (Path): Path to the directory with the PDBs to cluster
+        models_dir (Path): Path to the directory with the models from AlphaFold
         results_prefix (str, optional): Defaults to "clusterRes".
         temp_dir (str, optional): Defaults to "tmp".
         coverage (float, optional): Defaults to 0.8.
         cov_mode (int, optional): Defaults to 2.
         evalue (float, optional): Defaults to 0.01.
-        destination (Path, optional): Path to run the process from. Defaults to Path(".").
     """
     
-    command = (f"foldseek easy-cluster {models_dir} {results_prefix} {temp_dir} "
+    out_strcluster = destination / "strclusters"
+    out_strcluster.mkdir()
+
+    if top_models_dir is None:
+        logging.info("Copying models to new directory...")
+        pdbs_dir = get_top_pdbs(models_dir, out_strcluster)
+    else:
+        pdbs_dir = top_models_dir
+
+    logging.info("Running strcutural clustering...")
+    command = (f"foldseek easy-cluster {pdbs_dir} {results_prefix} {temp_dir} "
                f"-c {coverage} --cov-mode {cov_mode} -e {evalue}").split()
     
-    p = subprocess.run(command, cwd=destination)
+    p = subprocess.run(command, cwd=out_strcluster)
+    
+    logging.info("Processing output...")
+    strclusters_tsv = out_strcluster / "clusterRes_cluster.tsv"
+    strclusters = pd.read_table(strclusters_tsv, header=None, names=["rep", "member"])
+    # Get only the clusters for the structures of the binder
+    strclusters = strclusters[strclusters.member.str.endswith('_B')]
+    # See if all the representatives also come from chain B
+    if not all(strclusters.rep.str.endswith('_B')):
+        logging.info("NOT ALL REPRESENTATIVES COME FROM CHAIN B")
+    # Remove the '.pdb_B' suffix from the members' names
+    strclusters['member'] = strclusters['member'].str.split('.pdb').str[0]
+
+    return strclusters, pdbs_dir
+
+
+def add_quality_scores(strclusters:pd.DataFrame, seqclusters:pd.DataFrame,
+                       models_dir:Path) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Add the quality scores to the clusters
+
+    Args:
+        strclusters (pd.DataFrame): DataFrame with the structural clusters
+        seqclusters (pd.DataFrame): DataFrame with the sequence clusters
+        models_dir (Path): Path to the directory with the models from AlphaFold
+                           and pdockq scores
+
+    """
+    
+    logging.info("Adding quality scores to the clusters...")
+    pdockq = pd.read_csv(models_dir / "scores/scores_pdockq.csv")
+    pdockq['binder'] = pdockq.complex.str.split('_').str[1].str[:-2]
+
+    strclusters = pdockq.merge(strclusters, how='left', left_on='binder',
+                                right_on='member')
+
+    seqclusters = pdockq.merge(seqclusters, how='left', left_on='binder',
+                                right_on='member')
+    
+    return strclusters, seqclusters
 
 
 def get_top_pdbs(models_dir:Path, destination:Path):
@@ -185,7 +257,8 @@ def get_top_pdbs(models_dir:Path, destination:Path):
     return pdbs_dir
 
 
-def copy_pdbs(clusters:pd.DataFrame, pdbs_dir:Path, destination:Path) -> None:
+def copy_pdbs(clusters:pd.DataFrame, pdbs_dir:Path, destination:Path, top_n:int
+              ) -> None:
     """
     Copy the top pdbs from the top clusters to a new directory
     
@@ -194,12 +267,15 @@ def copy_pdbs(clusters:pd.DataFrame, pdbs_dir:Path, destination:Path) -> None:
             members
         pdbs_dir (Path): Path to the directory with the models
         destination (Path): Path to the directory where the pdbs will be copied
+        top_n (int): Number of top clusters to copy
     """
     
-    top10 = (clusters.groupby('merged_rep').agg({'iptms': 'mean', 'binder': 'count'})
-             .sort_values(by=['binder', 'iptms'], ascending=False).head(10))
+    topclusters = (clusters.groupby('merged_rep')
+                   .agg({'iptms': 'mean', 'binder': 'count'})
+                   .sort_values(by=['binder', 'iptms'], ascending=False)
+                   .head(top_n))
     
-    for i, cluster in enumerate(top10.index):
+    for i, cluster in enumerate(topclusters.index):
         # Get cluster members
         members = clusters[clusters.merged_rep == cluster].member.values
         cluster_dir = destination / f"cluster{i+1}_{cluster}"
@@ -211,12 +287,35 @@ def copy_pdbs(clusters:pd.DataFrame, pdbs_dir:Path, destination:Path) -> None:
             shutil.copy(pdb_name, new_name)
 
 
-def make_pymol_sessions(clusters:pd.DataFrame, destination:Path):
+def copy_merged_clusters(destination:Path, pdbs_dir:Path,
+                         strclusters:pd.DataFrame, top_n:int=10):
+    """
+    Copy the pdbs from the top clusters to a new directory, make pymol sessions
+    and write the scores to a csv file.
+
+    Args:
+        destination (Path): Output directory
+        pdbs_dir (Path): Path to the directory with the PDBs
+        strclusters (pd.DataFrame): DataFrame with the structural clusters
+        top_n (int, optional): Number of top clusters to copy. Defaults to 10.
+    """
+    out_merged = destination / "merged_clusters"
+    out_merged.mkdir()
+    logging.info("Copying pdbs from the top clusters...")
+    copy_pdbs(strclusters, pdbs_dir, out_merged, top_n)
     
-    top10 = (clusters.groupby('merged_rep').agg({'iptms': 'mean', 'binder': 'count'})
-             .sort_values(by=['binder', 'iptms'], ascending=False).head(10))
+    logging.info("Making Pymol sessions...")
+    make_pymol_sessions(strclusters, out_merged)
     
-    for i, cluster in enumerate(top10.index):
+    strclusters.to_csv(out_merged / "scores_clusters.csv")
+
+
+def make_pymol_sessions(clusters:pd.DataFrame, destination:Path, top_n:int):
+    
+    topclusters = (clusters.groupby('merged_rep').agg({'iptms': 'mean', 'binder': 'count'})
+             .sort_values(by=['binder', 'iptms'], ascending=False).head(top_n))
+    
+    for i, cluster in enumerate(topclusters.index):
         # Get cluster members
         members = clusters[clusters.merged_rep == cluster].member.values
         cluster_dir = destination / f"cluster{i+1}_{cluster}"
@@ -352,70 +451,20 @@ def joint_cluster(seqclusters:pd.DataFrame, strclusters:pd.DataFrame) -> Dict[st
             joint_clusters2[rep] = top_seqclusters_members[rep]
     
     return joint_clusters2
+
+
+def joint_clusters_df(seqclusters:pd.DataFrame, strclusters:pd.DataFrame
+                          ) -> pd.DataFrame:
+    """
+    Get the DataFrame with the joint clusters from the clusters of the structures
+    and the clusters of the sequences.
+    """
     
-
-
-if __name__ == '__main__':
-
-    args = parsing()
-    
-    # Sequence clustering
-    out_seqcluster = args.destination / "seqclusters"
-    out_seqcluster.mkdir()
-    logging.info("Running clustering...")
-    run_sequence_clustering(args.candidates, "clusterRes", "tmp", args.min_seq_id,
-                args.coverage, args.cov_mode, args.sensitivity, out_seqcluster)
-
-    logging.info("Processing output...")
-    seqclusters_tsv = out_seqcluster / "clusterRes_cluster.tsv"
-    seqclusters = pd.read_table(seqclusters_tsv, header=None, names=["rep", "member"])
-    
-    # Structure clustering
-    out_strcluster = args.destination / "strclusters"
-    out_strcluster.mkdir()
-
-    if args.top_models_dir is None:
-        logging.info("Copying models to new directory...")
-        pdbs_dir = get_top_pdbs(args.models_dir, out_strcluster)
-    else:
-        pdbs_dir = args.top_models_dir
-
-    logging.info("Running strcutural clustering...")
-    run_structure_clustering(pdbs_dir, "clusterRes", "tmp", args.coverage,
-                                args.cov_mode, args.evalue, out_strcluster)
-    
-    logging.info("Processing output...")
-    strclusters_tsv = out_strcluster / "clusterRes_cluster.tsv"
-    strclusters = pd.read_table(strclusters_tsv, header=None, names=["rep", "member"])
-    # Get only the clusters for the structures of the binder
-    strclusters = strclusters[strclusters.member.str.endswith('_B')]
-    # See if all the representatives also come from chain B
-    if not all(strclusters.rep.str.endswith('_B')):
-        logging.info("NOT ALL REPRESENTATIVES COME FROM CHAIN B")
-    # Remove the '.pdb_B' suffix from the members' names
-    strclusters['member'] = strclusters['member'].str.split('.pdb').str[0]
-
-    # Add quality scores to the clusters
-    logging.info("Adding quality scores to the clusters...")
-    pdockq = pd.read_csv(args.models_dir / "scores/scores_pdockq.csv")
-    pdockq['binder'] = pdockq.complex.str.split('_').str[1].str[:-2]
-
-    strclusters = pdockq.merge(strclusters, how='left', left_on='binder',
-                                right_on='member')
-
-    seqclusters = pdockq.merge(seqclusters, how='left', left_on='binder',
-                                right_on='member')
-
-    # Joint clustering
     joint_clusters = joint_cluster(seqclusters, strclusters)
     
-    # Modify old columns
-    strclusters.rename(columns={'rep':'str_rep'}, inplace=True)
-    seqclusters.rename(columns={'rep':'seq_rep'}, inplace=True)
-
-    # Make the `complex` column the index
-    strclusters.set_index('complex', inplace=True)
-    seqclusters.set_index('complex', inplace=True)
+    # Modify old columns and set `complex` as the index
+    strclusters = strclusters.rename(columns={'rep':'str_rep'}).set_index('complex')
+    seqclusters = seqclusters.rename(columns={'rep':'seq_rep'}).set_index('complex')
 
     # Add the `seq_rep` column to the `strclusters` dataframe
     strclusters['seq_rep'] = seqclusters.seq_rep
@@ -430,15 +479,30 @@ if __name__ == '__main__':
 
     strclusters.fillna(np.nan, inplace=True)
     
-    # Copy pdbs from the top clusters
-    out_merged = args.destination / "merged_clusters"
-    out_merged.mkdir()
-    logging.info("Copying pdbs from the top clusters...")
-    copy_pdbs(strclusters, pdbs_dir, out_merged)
+    return strclusters
+
+
+if __name__ == '__main__':
+
+    args = parsing()
     
-    logging.info("Making Pymol sessions...")
-    make_pymol_sessions(strclusters, out_merged)
+    seqclusters = run_sequence_clustering(args.destination, args.candidates,
+                        results_prefix="clusterRes", temp_dir="tmp",
+                        min_seq_id=args.min_seq_id, coverage=args.coverage,
+                        cov_mode=args.cov_mode, sensitivity=args.sensitivity)
+    
+    strclusters, pdbs_dir = run_structure_clustering(args.destination,
+                        args.top_models_dir, args.models_dir,
+                        results_prefix="clusterRes", temp_dir="tmp",
+                        coverage=args.coverage, cov_mode=args.cov_mode,
+                        evalue=args.evalue)
+    
+    strclusters, seqclusters = add_quality_scores(strclusters, seqclusters,
+                                                  args.models_dir)
+    
+    strclusters = joint_clusters_df(seqclusters, strclusters)
 
-    strclusters.to_csv(out_merged / "scores_clusters.csv")
 
-    logging.info("Done!!")
+    # Copy pdbs from the top clusters and make pymol sessions
+    copy_merged_clusters(args.destination, pdbs_dir, strclusters, args.top_n)
+    
